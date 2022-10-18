87bf7a5f66847cbc7ec01c72386b5421fbe5eb64
SziNo <szino1848@gmail.com>
2022-10-18 15:25:38 +0200

    adjusting TS logo img

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import react from '../assets/react.png'
 import node from '../assets/node.png'
 import mongo from '../assets/mongo.png'
 import tailwind from '../assets/tailwind.png'
-import tsa from '../assets/tsa.png'
+import ts from '../assets/ts.png'
 
 const Skills = () => {
   return (
@@ -39,7 +39,7 @@ const Skills = () => {
             <p className='my-4'>JavaScript</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={tsa} alt='TypeScript icon' />
+            <img className='w-20 mx-auto' src={ts} alt='TypeScript icon' />
             <p className='my-4'>TypeScript</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
