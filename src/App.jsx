f3a1f11c2b59cc9ce5f0cfb4a081b1fc5702f77c
SziNo <szino1848@gmail.com>
2023-04-27 23:40:44 +0200

    updating App to have proper jsx extension

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from './pages/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Skills from './pages/Skills'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  )
+}
+export default App
