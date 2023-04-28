d38d7c9c96a1fa89a6a799f88368247ceddc20c7
SziNo <szino1848@gmail.com>
2023-04-28 20:50:08 +0200

    getArrayData function partly done

---
+++ src/utils/getArrayData.js
@@ -0,0 +1,9 @@
+export const getArrayData = () => {
+  const socialIconNames = ['LinkedIn', 'GitHub', 'Email']
+  const navbarLinkNames = ['Home', 'About', 'Skills', 'Projects', 'Contact']
+
+  return {
+    socialIconNames,
+    navbarLinkNames,
+  }
+}
