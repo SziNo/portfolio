a8f8a5d496e696ca5b55441b3f1d274dded84d9d
SziNo <szino1848@gmail.com>
2023-04-29 11:33:58 +0200

    fix: added safelist to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: ['bg-blue-800', 'bg-gray-800', 'bg-red-800'],
   theme: {
     extend: {},
   },
